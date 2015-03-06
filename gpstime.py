0275d49d9147cc7028a32b851172b25f1fbcd9b3
Gunaydin <appie_96@live.com>
2015-03-07 00:18:59 +0100

    Update gpstime.py
    New script. Can also be used for calculating the ltime. To calculate the htime, use the script in the opposite way.

--- gpstime.py
+++ gpstime.py
@@ -11,3 +11,11 @@ for entry in tree:
 		htime = entry.GpsTime
 		print("htime")
 		print(htime)
+#This script can also be used
+#from ROOT import *
+#tree = TFile.Open("root_real_data.root").Get("DecayTree")
+#ltime = 99999999999999999999999999999999999999999999999999999999999999999999999999
+#for entry in tree:
+#	if entry.GpsTime < ltime:
+#		ltime = entry.GpsTime
+#print(ltime)
