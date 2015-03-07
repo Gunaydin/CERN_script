fcd594440c7631967f1d6d9711edb53472b00fac
Arkiver2 <Arkiver@hotmail.com>
2015-03-07 16:57:29 +0100

    gpstime2.py: a tip.

--- gpstime2.py
+++ gpstime2.py
@@ -1,11 +1,7 @@
 from ROOT import *
 ltime = 1302828296975552
 htime = 1319760301662592
-a=0
-if a==0:
-	difunixtime = htime - ltime
-	partunixtime = difunixtime/12
-	b=ltime+partunixtime
-	print(b)
-	if b==1319760301662592:
-		c=1
+difunixtime = htime - ltime
+partunixtime = difunixtime/12
+while not b == 1319760301662592:
+	.....
